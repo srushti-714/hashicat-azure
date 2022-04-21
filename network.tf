e664b7bf51d0af486bee66a96c1b78ace406b379
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2022-04-21 18:07:02 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -4,7 +4,6 @@ module "network" {
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
   tags = {
     Department = "devops"
     Billable = "false"
