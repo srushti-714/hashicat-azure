5167174496aaa83f41d820338f0960a70e843842
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2022-04-21 17:30:39 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,15 +1,9 @@
 module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  source              = "app.terraform.io/srustiorg/network/azurerm"
+  resource_group_name = "${var.prefix)_workshop"
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  subnet_service_endpoints = {
-    "subnet1" : ["Microsoft.Sql"], 
-    "subnet2" : ["Microsoft.Sql"],
-    "subnet3" : ["Microsoft.Sql"]
-  }
 
   tags = {
     Department = "devops"
